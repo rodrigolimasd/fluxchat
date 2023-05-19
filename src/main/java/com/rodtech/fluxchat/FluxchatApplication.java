c1006417f7788ab96c3796b970823903e3fe99b1
Rodrigo Lima <rodrigolimasd@gmail.com>
2023-05-18 22:34:44 -0300

    firt commit

---
+++ src/main/java/com/rodtech/fluxchat/FluxchatApplication.java
@@ -0,0 +1,13 @@
+package com.rodtech.fluxchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodtech/fluxchat/FluxchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodtech.fluxchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
