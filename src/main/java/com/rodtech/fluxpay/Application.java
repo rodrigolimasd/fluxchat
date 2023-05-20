22747bf138d58a697ea05003f96696d03a7f6f88
Rodrigo Lima <rodrigolimasd@gmail.com>
2023-05-19 23:24:50 -0300

    feat: rename project

--- src/main/java/com/rodtech/fluxpay/Application.java
+++ src/main/java/com/rodtech/fluxpay/Application.java
@@ -1,13 +1,13 @@
-package com.rodtech.fluxchat;
+package com.rodtech.fluxpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FluxchatApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FluxchatApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
