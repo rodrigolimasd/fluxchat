4fe58895e6ded916784d4f37ebaff9145324f4d6
Rodrigo Lima <rodrigolimasd@gmail.com>
2023-06-03 19:06:24 -0300

    feat: adjusts

--- src/test/java/com/rodtech/fluxpay/FluxpayApplicationTests.java
+++ src/test/java/com/rodtech/fluxpay/FluxpayApplicationTests.java
@@ -3,11 +3,11 @@ package com.rodtech.fluxpay;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FluxpayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class FluxpayApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
