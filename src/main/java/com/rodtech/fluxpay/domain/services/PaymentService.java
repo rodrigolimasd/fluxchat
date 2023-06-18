033dacea4a4c16f04a34b4331f44a73dc88dbfad
Rodrigo Lima <rodrigolimasd@gmail.com>
2023-06-18 12:14:38 -0300

    feat: implemented update payment status

--- src/main/java/com/rodtech/fluxpay/domain/services/PaymentService.java
+++ src/main/java/com/rodtech/fluxpay/domain/services/PaymentService.java
@@ -1,10 +1,12 @@
 package com.rodtech.fluxpay.domain.services;
 
 import com.rodtech.fluxpay.domain.models.payment.Payment;
+import com.rodtech.fluxpay.domain.models.payment.enums.PaymentStatus;
 
 import java.util.UUID;
 
 public interface PaymentService {
     Payment pay(Payment payment);
     Payment get(UUID id);
+    void updateStatus(PaymentStatus status, UUID id);
 }
--- src/main/java/com/rodtech/fluxpay/domain/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/rodtech/fluxpay/domain/services/impl/PaymentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.rodtech.fluxpay.domain.services.impl;
 
+import com.rodtech.fluxpay.domain.exceptions.PaymentValidationException;
 import com.rodtech.fluxpay.domain.gateways.payment.PaymentDataGateway;
 import com.rodtech.fluxpay.domain.gateways.payment.PaymentGateway;
 import com.rodtech.fluxpay.domain.models.payment.Payment;
@@ -39,5 +40,23 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public Payment get(UUID id) {
         return paymentDataGateway.getById(id);
+    }
+
+    @Override
+    public void updateStatus(PaymentStatus status, UUID id) {
+        var payment = paymentDataGateway.getById(id);
+
+        validateStatus(payment, status);
+        payment.setStatus(status);
+        paymentDataGateway.updateStatus(status, id);
+    }
+
+    private void validateStatus(Payment payment, PaymentStatus status) {
+        if(payment.getStatus()!=PaymentStatus.PENDING) {
+            throw new PaymentValidationException("Invalid update status - paymentId: "+payment.getId());
+        }
+        if(status==PaymentStatus.PENDING) {
+            throw new PaymentValidationException("Invalid status - status: "+ status);
+        }
     }
 }
